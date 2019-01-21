4449813d9df49a3c1436b5fa1c1a2beb03404140
luvm <43397148+luvm@users.noreply.github.com>
2019-01-21 16:08:00 +0800

    Add files via upload
    研究生英语读写译

---
+++ english_class.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+import time
+import random
+
+def login():
+    driver.get('http://spoc.ccnu.edu.cn/starmoocHomepage')
+
+    driver.find_element_by_id("loginName").clear()
+    driver.find_element_by_id("loginName").send_keys("2018171143")
+    driver.find_element_by_id("password").clear()
+    driver.find_element_by_id("password").send_keys("2018171143")
+    driver.find_element_by_id('loginBtn').click()
+
+def class_chose():
+    time.sleep(2)
+    driver.find_element_by_xpath('/html/body/div[4]/div/button').click()
+    time.sleep(2)
+    driver.find_element_by_class_name('border').click()
+    time.sleep(2)
+
+def swith_close(n):
+    print('第%d次开始' % n)
+    # time.sleep(2)
+    time.sleep(random.randint(600,1200))
+    print('第%d次结束' % n)
+    driver.switch_to_window(driver.window_handles[1])
+    driver.close()
+    driver.switch_to_window(driver.window_handles[0])
+
+def time_getter():
+    n = 0
+
+    login()
+
+    class_chose()
+
+    list1 = []
+    for t in range(1,9):
+        if t == 1:
+            a = 3
+        else:
+            a = 2
+        if t == 7:
+            b = 2
+        else:
+            b = 1
+        driver.find_element_by_xpath('//*[@id="test"]/div/div/div/div/div/div/div[{}]/div/div/div[{}]/h4/i'.format(t,a)).click()
+        driver.find_element_by_xpath('//*[@id="test"]/div/div/div/div/div/div/div[{}]/div/div/div[{}]/div/div/div[1]/h4/i'.format(t,a)).click()
+        driver.find_element_by_xpath('//*[@id="test"]/div/div/div/div/div/div/div[{}]/div/div/div[{}]/div/div/div[1]/div/div/div[{}]/h4/i'.format(t,a,b)).click()
+        list2 = driver.find_elements_by_xpath('//*[@id="test"]/div/div/div/div/div/div/div[{}]/div/div/div[{}]/div/div/div[1]/div/div/div[{}]/div/div/div/h4/i'.format(t,a,b))
+        for i,li in enumerate(list2,1):
+            li.click()
+            li.find_element_by_xpath('//*[@id="test"]/div/div/div/div/div/div/div[{}]/div/div/div[{}]/div/div/div[1]/div/div/div[{}]/div/div/div[{}]/div/div/div[1]/a/span'.format(t,a,b,i)).click()
+            n += 1
+            swith_close(n)
+    driver.quit()
+
+if __name__ == '__main__':
+    m = 1
+    while True:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('-ignore-certificate-errors')
+        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.binary_location = (r'C:\Users\jack8\AppData\Local\Google\Chrome\Application\chrome.exe')
+        driver = webdriver.Chrome(options=chrome_options)
+
+        print('第%d轮循环开始'%m)
+        time_getter()
+        m += 1
