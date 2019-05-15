b41eb5458a9b49ccc8759f31f21da4f888e030dc
luvm <43397148+luvm@users.noreply.github.com>
2019-05-15 13:05:32 +0800

    又有新课了

---
+++ class_class.py
@@ -0,0 +1,66 @@
+from selenium import webdriver
+import time
+import random
+
+import pytesseract
+from PIL import Image
+
+import base64
+
+# pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
+filePath = './pic.png'
+
+def login():
+    driver.get('http://spoc.ccnu.edu.cn/starmoocHomepage')
+    driver.implicitly_wait(10)
+    driver.find_element_by_id("loginName").clear()
+    driver.find_element_by_id("loginName").send_keys("2018171143")
+    driver.find_element_by_id("password").clear()
+    driver.find_element_by_id("password").send_keys("2018171143")
+    src = driver.find_element_by_id('verifCodeImg').get_attribute('src').split(',',1)[1]
+    img=base64.b64decode(src)
+    with open(filePath,'wb') as f:
+        f.write(img)
+    image_en = Image.open('pic.png')
+    text_en = pytesseract.image_to_string(image_en)
+    driver.find_element_by_id('verifCode').send_keys(text_en)
+    driver.find_element_by_id('loginBtn').click()
+
+def class_chose():
+    time.sleep(2)
+    driver.find_element_by_xpath('//*[@id="s-h-course-info"]/div[2]/div').click()
+    time.sleep(2)
+
+def switch_close(n):
+    print('第%d次开始' % n)
+    # time.sleep(2)
+    time.sleep(random.randint(600,1200))
+    print('第%d次结束' % n)
+    driver.switch_to.window(driver.window_handles[1])
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
+
+def time_getter():
+
+    login()
+
+    class_chose()
+
+    url_li = driver.find_elements_by_xpath('//*[contains(@class,"icon-wmv") or contains(@class,"icon-mkv")]')
+    for n,i in enumerate(url_li):
+        i.click()
+        switch_close(n+1)
+    driver.quit()
+
+if __name__ == '__main__':
+    m = 1
+    while True:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('-ignore-certificate-errors')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        driver = webdriver.Chrome(options=chrome_options)
+
+        print('第%d轮循环开始'%m)
+        time_getter()
+        m += 1
